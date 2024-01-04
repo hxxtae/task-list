72143cb783df19d1413e93bfefdc60e70f07d015
hxxtae <fkdlxmfkdl@gmail.com>
2024-01-04 23:54:33 +0900

    :books: [Docs]: Apply Category & CategoryModal

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import Category from './Category';
 
 export default function App() {
+  const [task, setTask] = useState(1);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Category id={task} />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +17,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
 });
+
+// [Design]
+// ref: https://dribbble.com/shots/5985329-Do-More-Task-List
+
+// [UI]
+// ref: https://velog.io/@usaindream/React-Native-UI-%EA%B4%80%EB%A0%A8
