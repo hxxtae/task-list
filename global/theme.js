008d8b319125a72b1bb4183cc04739f8e9b7776a
hxxtae <fkdlxmfkdl@gmail.com>
2024-01-16 23:53:44 +0900

    :book: [feat]: Create Theme on global

---
+++ global/theme.js
@@ -0,0 +1,19 @@
+import { DefaultTheme, DarkTheme } from '@react-navigation/native';
+
+export const LightThemes = {
+  ...DefaultTheme,
+  primary: '#597EDB',
+  secondary: '#BCBCBC',
+  background: '#FFFFFF',
+  text: '#111111',
+  card: '#E7E7E7',
+}
+
+export const DarkThemes = {
+  ...DarkTheme,
+  primary: '#597EDB',
+  secondary: '#767676',
+  background: '#111111',
+  text: '#ffffff',
+  card: '#121519',
+}
