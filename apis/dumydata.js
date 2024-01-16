f39757d72a229617513f9bad6dbad73d1e9166f6
hxxtae <fkdlxmfkdl@gmail.com>
2024-01-16 23:52:24 +0900

    :book: [feat]: Create task fetch api

---
+++ apis/dumydata.js
@@ -0,0 +1,92 @@
+export const initData = {
+  C_190581: {
+    title: "Work",
+    list: {
+      262968: {
+        name: "Chicken",
+        date: "1705327291051",
+        check: false
+      },
+      451807: {
+        name: "Vegetable moodles",
+        date: "1705327291051",
+        check: false
+      },
+      512224: {
+        name: "Fresh Asparagus",
+        date: "1705327291051",
+        check: false
+      },
+      513392: {
+        name: "Wine",
+        date: "1705327291051",
+        check: false
+      },
+      678180: {
+        name: "Salmon",
+        date: "1705327291051",
+        check: false
+      },
+      800458: {
+        name: "Boneless chicken breasts",
+        date: "1705327261619",
+        check: false
+      },
+      958360: {
+        name: "Diapers",
+        date: "1705327291051",
+        check: false
+      },
+      971118: {
+        name: "Broccoli",
+        date: "1705327291051",
+        check: false
+      }
+    }
+  },
+  C_411846: {
+    title: "Business",
+    list: {
+      262968: {
+        name: "Chicken 2",
+        date: "1705327291051",
+        check: false
+      },
+      451807: {
+        name: "Vegetable moodles 2",
+        date: "1705327291051",
+        check: false
+      },
+      512224: {
+        name: "Fresh Asparagus 2",
+        date: "1705327291051",
+        check: false
+      },
+      513392: {
+        name: "Wine 2",
+        date: "1705327291051",
+        check: false
+      },
+      678180: {
+        name: "Salmon 2",
+        date: "1705327291051",
+        check: false
+      },
+      800458: {
+        name: "Boneless chicken breasts 2",
+        date: "1705327261619",
+        check: false
+      },
+      958360: {
+        name: "Diapers 2",
+        date: "1705327291051",
+        check: false
+      },
+      971118: {
+        name: "Broccoli 2",
+        date: "1705327291051",
+        check: false
+      }
+    }
+  }
+}
---
+++ apis/model.js
@@ -0,0 +1,43 @@
+import { initData } from './dumydata';
+import AsyncStorageStatic from '@react-native-async-storage/async-storage';
+
+export const fetchTaskData = async () => {
+  try {
+    const data = (await fetch('https://reactnative.dev/movies.json', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })).json();
+
+    return data;
+  } catch (e) {
+    console.error(`Fetch Error: ${e.message}`);
+  }
+}
+
+/*
+-----------------------------
+  React Native LocalStorage
+-----------------------------
+ */
+// 스토리지에 Task 데이터 저장
+export const setStorage = async (data) => {
+  AsyncStorageStatic.setItem('myTask', JSON.stringify(data));
+}
+
+// 스토리지에서 Task 데이터 불러오기
+export const getStorage = async () => {
+  return  JSON.parse(await AsyncStorageStatic.getItem('myTask'));
+}
+
+// 스토리지 초기화
+// - key: 'myTask'
+// - value: {}
+export const initSetStorage = async () => {
+  await AsyncStorageStatic.setItem('myTask', JSON.stringify(initData));
+}
+
+
+
